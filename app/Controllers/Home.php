da9e1356242ccee4c6122a281cea66fe50cf02c1
unknown <fajarainunyahya94@gmail.com>
2023-10-14 10:42:52 +0700

    fixing routes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     { //define("ENVIRONMENT","development");
-        return view('welcome_message');
+        return view('user/index');
     }
     public function pages()
     {
---
+++ app/Views/user/index.php
@@ -0,0 +1,3 @@
+<h1>Hello, world!</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam vero repellat quaerat? Delectus odio consequuntur, vero ut, repudiandae deserunt quod, tenetur labore molestiae incidunt esse similique mollitia! Labore, eum delectus.</p>
