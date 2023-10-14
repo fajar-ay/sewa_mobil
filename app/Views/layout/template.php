d5851e2f9e5d8128aedafa476643e06c1d8f128a
unknown <fajarainunyahya94@gmail.com>
2023-10-14 15:09:36 +0700

    layouting

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -7,30 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="/css/tailwind.css" rel="stylesheet">
     <title>Hello, world!</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Book now</a>
-                    <a class="nav-item nav-link" href="#">About</a>
-                    <a class="nav-item nav-link" href="#">Contact</a>
-
-                </div>
-            </div>
-        </div>
-    </nav>
-
+    <?= $this->include('layout/navbar'); ?>
+    <?= $this->renderSection('content'); ?>
 
 
     <!-- Optional JavaScript -->
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -1,3 +1,21 @@
-<h1>Hello, world!</h1>
+<?= $this->extend('layout/template'); ?>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam vero repellat quaerat? Delectus odio consequuntur, vero ut, repudiandae deserunt quod, tenetur labore molestiae incidunt esse similique mollitia! Labore, eum delectus.</p>
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello, world!</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam vero repellat quaerat? Delectus odio consequuntur, vero ut, repudiandae deserunt quod, tenetur labore molestiae incidunt esse similique mollitia! Labore, eum delectus.</p>
+            <p>ini untuk vue setelah ini</p>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.3.4/vue.cjs.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.3.4/vue.cjs.min.js" integrity="sha512-Nk4aRxW2zg0jN2rFYzF1Hc/xiTNTkk1LwMytaVrL7hK4Qij51+H7oNmKzDDYdUQzEEWAgBhzQ7F6bGYesDi1Bw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+            <script>
+                new Vue({
+
+                });
+            </script>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
